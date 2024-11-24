feadbd6a255fc2a0d6140fb3806a7a3a473a4c4c
Håkan Olsson <hakan.olsson@nexusgroup.com>
2024-11-24 15:56:21 +0100

    Fix byte format bug and base64

--- Orient/misc.js
+++ Orient/misc.js
@@ -1,20 +1,27 @@
-export function toBase64URL(bytes) {
-    const base64Encoded = btoa(bytes);
-    return base64Encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-}
-  
-export function fromBase64URL(str) {
-    const base64Encoded = str.replace(/-/g, '+').replace(/_/g, '/');
-    const padding = str.length % 4 === 0 ? '' : '='.repeat(4 - (str.length % 4));
-    const base64WithPadding = base64Encoded + padding;
-    return atob(base64WithPadding)
-}
-
-export function fromHexString(hexString){
-    return Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+export function fromHexString(hex){
+    return Uint8Array.from(hex.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
 }
 
 export function toHexString(bytes){
     return bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+}
+
+export function toBase64(bytes) {
+    return btoa(String.fromCharCode.apply(null, bytes));
+}
+  
+export function fromBase64(base64) {
+    return Uint8Array.from(atob(base64), c => c.charCodeAt(0))
+}
+
+export function toBase64URL(bytes) {
+    const base64Encoded = toBase64(bytes);
+    return base64Encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+  
+export function fromBase64URL(base64) {
+    const base64Encoded = base64.replace(/-/g, '+').replace(/_/g, '/');
+    const padding = base64.length % 4 === 0 ? '' : '='.repeat(4 - (base64.length % 4));
+    return fromBase64(base64Encoded + padding)
 }
   
